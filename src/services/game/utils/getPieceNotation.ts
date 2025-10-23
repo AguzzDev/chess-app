50f8d2864abbf9f5b5b0556712a68705f9b1ada5
AguzzDev <aribotta1901@gmail.com>
2025-10-23 11:45:13 -0300

    fix: unspecified case in getPieceNotation

--- src/services/game/utils/getPieceNotation.ts
+++ src/services/game/utils/getPieceNotation.ts
@@ -1,7 +1,8 @@
 import { PieceTypeEnum } from "@/interfaces";
 
-export const getPieceNotation = (piece: PieceTypeEnum) => {
+export const getPieceNotation = (piece: PieceTypeEnum): string => {
   const PIECE_DICT = {
+    [PieceTypeEnum.empty]: "",
     [PieceTypeEnum.pawn]: "",
     [PieceTypeEnum.horse]: "N",
     [PieceTypeEnum.bishop]: "B",
